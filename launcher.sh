2ddc4f47dfc43683d1f3a2be4811c4cd4ef95625
SLAC fphysics <zack@slac.stanford.edu>
2024-10-11 12:44:13 -0700

    add launcher script

---
+++ launcher.sh
@@ -0,0 +1,11 @@
+# /usr/bin/bash
+if [[ $1 == "lcls" ]]; then
+	HOSTNAME="physics@lcls-srv02"
+elif [[ $1 == "facet" ]]; then
+    HOSTNAME="fphysics@facet-srv02"
+else
+	echo " --> Bad arguments, must specify 'lcls' or 'facet'"
+    exit 0
+fi
+
+ssh $HOSTNAME "pydm --hide-nav-bar --hide-status-bar --hide-menu-bar /home/fphysics/zack/workspace/rtbsa2/rtbsaGUI.py"
